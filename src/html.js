d8a49824b5e55e51b1e5bea4cc8c3748abb9d2da
ajurasz <arkadiusz.jurasz@gmail.com>
2020-11-18 19:43:46 +0100

    Add google-site-verification

--- src/html.js
+++ src/html.js
@@ -13,6 +13,7 @@ export default class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
+          <meta name="google-site-verification" content="CcQWHZSra56gJoiG2L4xv8GjL2z_C1txoouzKlR4ASk" />
           {this.props.headComponents}
           <link rel="shortcut icon" href={favicon} />
         </head>
